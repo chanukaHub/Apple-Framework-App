4a2a141f835640f36b4df5ec12991a0bd40266ce
Chanuka Dilhara Mullevidana <62568997+chanukaHub@users.noreply.github.com>
2023-11-05 16:54:56 +0530

    initial commit

---
+++ Apple Framework/Framework.swift
@@ -0,0 +1,8 @@
+//
+//  Framework.swift
+//  Apple Framework
+//
+//  Created by Chanuka Mullevidana on 2023-11-05.
+//
+
+import Foundation
---
+++ Apple Framework/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple Framework
+//
+//  Created by Chanuka Mullevidana on 2023-11-05.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
