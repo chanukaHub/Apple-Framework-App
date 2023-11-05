1a5040f13f48a064622359410df5823782418ddc
Chanuka Dilhara Mullevidana <62568997+chanukaHub@users.noreply.github.com>
2023-11-05 16:25:27 +0530

    Initial Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Chanuka Mullevidana on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Framework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Framework
+//
+//  Created by Chanuka Mullevidana on 2023-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
