e65a67e7c1a5e36ac240eda79b4a8b3b0d274b72
Chanuka Dilhara Mullevidana <62568997+chanukaHub@users.noreply.github.com>
2023-11-06 08:50:28 +0530

    added framework details page

---
+++ Apple Framework/AFButton.swift
@@ -0,0 +1,26 @@
+//
+//  AFButton.swift
+//  Apple Framework
+//
+//  Created by Chanuka Mullevidana on 2023-11-05.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    AFButton(title: "Test Text")
+}
